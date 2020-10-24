4fb05a161052840b4284817196568d35bccb2c27
jotd <jotd@orange.fr>
2020-10-24 18:32:31 +0200

    full cdtv rom patch

--- tools/patch_cdtv_rom.py
+++ tools/patch_cdtv_rom.py
@@ -22,9 +22,47 @@ h = hashlib.md5(contents).hexdigest()
 # 1.0
 # 2.3 d98112f18792ee3714df16a6eb421b89
 
+plist = (
+(0x00000452 , 0x0002),
+(0x00000504 , 0x0002),
+(0x000008b2 , 0x0002),
+(0x000008f4 , 0x0002),
+(0x00000df0 , 0x0002),
+(0x00001a7e , 0x0002),
+(0x00001e64 , 0x0002),
+(0x00007662 , 0x0002),
+(0x000092e4 , 0x0002),
+(0x00009342 , 0x0002),
+(0x00009446 , 0x0002),
+(0x0000a740 , 0x0002),
+(0x0000a872 , 0x0002),
+(0x0000ae12 , 0x0002),
+(0x0000d372 , 0x0002),
+(0x0000d472 , 0x0002),
+(0x00038706 , 0x0002),
+(0x00038d86 , 0x0002),
+(0x00000db8 , 0x7202),
+(0x0000407c , 0x7202),
+(0x000089ba , 0x7202),
+(0x0000a004 , 0x7202),
+(0x0000a168 , 0x7202),
+(0x0000ab54 , 0x7202),
+(0x0000acca , 0x7202),
+(0x0000eef6 , 0x7202),
+(0x0000ef10 , 0x7202),
+(0x0000ef2a , 0x7202),
+(0x0000ef42 , 0x7202),
+(0x00010e88 , 0x7202),
+(0x0003b232 , 0x7202),
+)
+
 if h == "d98112f18792ee3714df16a6eb421b89":
     print("2.3 original ROM, patching...")
-    contents[0x38707] = 2
+    for offset,word in plist:
+        msb = (word >> 8)
+        lsb = (word & 0xFF)
+        contents[offset] = msb
+        contents[offset+1] = lsb
     with open(outfile,"wb") as f:
         f.write(contents)
 elif h == "89da1838a24460e4b93f4f0c5d92d48d":
@@ -35,7 +73,7 @@ elif h == "fc7f68c6df62b494574c25520226a326":
     with open(outfile,"wb") as f:
         f.write(contents)
 
-elif h == "27df2662a0539fbf48235f2837f00e19":
+elif h == "94799fb3444a305965ea73aceb7348fa":
     print("2.3 patched rom, doing nothing")
 elif h == "78e2695573d403edb92e0cd9a67bc653":
     print("cdfs.library already patched, doing nothing")
