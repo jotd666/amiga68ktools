0522937bfec2966cfdc8e41576ee0540b514cd27
jotd <jotd@orange.fr>
2020-10-24 10:06:48 +0200

    CDTV 2.3 ROM patcher

---
+++ tools/patch_cdtv_rom.py
@@ -0,0 +1,36 @@
+import sys,re,os,hashlib
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input_file")
+parser.add_argument("output_file")
+
+
+args = parser.parse_args()
+
+infile = args.input_file
+outfile = args.output_file
+if infile == outfile:
+    print("you don't want to overwrite your original file, choose another name")
+    sys.exit(1)
+
+with open(infile,"rb") as f:
+    contents = bytearray(f.read())
+
+h = hashlib.md5(contents).hexdigest()
+
+# 1.0
+# 2.3 d98112f18792ee3714df16a6eb421b89
+
+if h == "d98112f18792ee3714df16a6eb421b89":
+    print("2.3 original ROM, patching...")
+    contents[0x38707] = 2
+    with open(outfile,"wb") as f:
+        f.write(contents)
+elif h == "89da1838a24460e4b93f4f0c5d92d48d":
+    print("1.0 ROM, unsupported")
+elif h == "27df2662a0539fbf48235f2837f00e19":
+    print("2.3 patched rom, doing nothing")
+else:
+    print("unknown version MD5 {}".format(h))
+
